a0cfdc528b380cd17a4e64b404799e2fcea93c2f
João Vitor Girardi <Jvitorstahl@hotmail.com>
2021-11-07 12:00:55 -0300

    first request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,24 +1,45 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+type ChartData = {
+    series: number[];
+    labels: string[];
+}
 const DonutChart = () => {
+
+    let chartData : ChartData = { series: [], labels: [] };
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(resp => {
+           const data = resp.data as SaleSum[];
+           const myLabels = data.map(x => x.sellerName);
+           const mySeries = data.map(x => x.sum);
+           chartData =  { series: mySeries, labels: myLabels};    
+
+
+            console.log(chartData);
+        } );
+    /*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+    */
+
     const options = {
         legend: {
             show: true
         }
     }
-    
+
 
     return (
-        <Chart 
-            options = {{...options, labels: mockData.labels}}
-            series = {mockData.series}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
-        />    
+        />
     );
 }
 
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -12,11 +12,11 @@ const Dashboard = () => {
                 <h1 className="text-primary py-3">Relatório de vendas!</h1>
                 <div className="row px-3">
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+                        <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+                        <h5 className="text-center text-secondary">Todas Vendas</h5>
                         <DonutChart />
                     </div>
                 </div>
